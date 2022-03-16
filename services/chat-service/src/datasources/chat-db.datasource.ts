610f1215f1d259c51b55f2f067d6d5610bc44c2b
Bhanu Manthry <bhanuprasad.manthry@sourcefuse.com>
2022-03-16 23:29:38 +0530

    Add chat-db data source.

---
+++ services/chat-service/src/datasources/chat-db.datasource.ts
@@ -0,0 +1,31 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'chatDb',
+  connector: 'postgresql',
+  url: 'postgres://admin:password@localhost/chat',
+  host: 'localhost',
+  port: 5432,
+  user: 'admin',
+  password: 'password',
+  database: 'chat'
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class ChatDbDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'chatDb';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.chatDb', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
